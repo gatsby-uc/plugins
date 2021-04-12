9cca957eae03d5f11cb2231034f8255c894df74c
Alex Moon <moonmeister@users.noreply.github.com>
2021-04-12 11:55:08 -0500

    chore(plugin-relative): new plugin (#12)
    * initial
    
    * initial
    
    * v1.0.0
    
    * pass options

---
+++ packages/gatsby-plugin-relative-ci/src/gatsby-node.js
@@ -0,0 +1,9 @@
+const { RelativeCiAgentWebpackPlugin } = require("@relative-ci/agent")
+
+export const onCreateWebpackConfig = ({ stage, actions }, pluginOptions) => {
+  if (stage === "build-javascript") {
+    actions.setWebpackConfig({
+      plugins: [new RelativeCiAgentWebpackPlugin(pluginOptions)],
+    })
+  }
+}
