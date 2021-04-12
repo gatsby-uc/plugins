bed7dc6283d0e8abda322b8fa16cb83d913f5ee0
Alex Moon <moonmeister@users.noreply.github.com>
2021-04-12 10:34:19 -0500

    chore(gatsby-source-packagist): merge into repo (#10)
    * initial source-packagist copmmit
    
    * fix gitignore
    
    * update packages
    
    * cleanup
    
    * formatting
    
    * update version for management bby auto release
    
    * fix version

---
+++ packages/gatsby-plugin-github-ribbon/src/__tests__/gatsby-ssr.js
@@ -0,0 +1,32 @@
+import { onRenderBody } from "../gatsby-ssr"
+
+describe(`adds github ribbon to html`, () => {
+  it.each`
+    position
+    ${`left`}
+    ${`right`}
+  `(
+    `create html for injecting if config is set - $position`,
+    ({ position }) => {
+      //setup test data
+      const pluginOptions = {
+        project: `https://github.com/gatsbyjs/gatsby`,
+        color: `red`,
+        position: position,
+      }
+
+      const setPreBodyComponents = jest.fn()
+
+      //fake test
+      onRenderBody(
+        {
+          setPreBodyComponents,
+        },
+        pluginOptions
+      )
+
+      expect(setPreBodyComponents).toMatchSnapshot()
+      expect(setPreBodyComponents).toHaveBeenCalledTimes(1)
+    }
+  )
+})
