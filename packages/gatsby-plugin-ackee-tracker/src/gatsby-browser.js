10aabf92fe3169236fe5836b825182220490459a
Alex Moon <moonmeister@users.noreply.github.com>
2021-09-14 11:43:15 -0700

    add: gatsby-plugin-ackee-tracker (#55)
    * Initial move of plugin
    
    * Create gold-experts-carry.md

---
+++ packages/gatsby-plugin-ackee-tracker/src/gatsby-browser.js
@@ -0,0 +1,34 @@
+import ackeeTracker from "ackee-tracker"
+
+/**
+ * Hold the tracker instance. Persists after the client is hydrated,
+ * so is destroyed when any page is re-fetched from the server.
+ */
+let trackerInstance
+
+/**
+ * Record a page visit when it is visited.
+ *
+ * @param {*} _ - unused object with parameters `location` and `prevLocation`.
+ * @param {*} pluginOptions - contains all options as defined by the config.
+ */
+export const onRouteUpdate = (
+  _,
+  { domainId, server, ignoreLocalhost, ignoreOwnVisits, detailed }
+) => {
+  /**
+   * If there is no tracker instance, instantiate one with the plugin options.
+   */
+  if (trackerInstance == null) {
+    trackerInstance = ackeeTracker.create(server, {
+      ignoreLocalhost,
+      ignoreOwnVisits,
+      detailed,
+    })
+  }
+
+  /**
+   * Record the visit to this route.
+   */
+  trackerInstance.record(domainId)
+}
---
+++ packages/gatsby-plugin-ackee-tracker/src/gatsby-node.js
@@ -0,0 +1,9 @@
+export const pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    domainId: Joi.string().guid({ version: "uuidv4" }).required(),
+    server: Joi.string().uri().required(),
+    ignoreLocalhost: Joi.boolean().default(true),
+    ignoreOwnVisits: Joi.boolean().default(false),
+    detailed: Joi.boolean().default(false),
+  })
+}
