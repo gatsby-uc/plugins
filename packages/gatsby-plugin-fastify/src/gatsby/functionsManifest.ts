09bb2c272f7127a732e1ad54d2b8fb38db054523
Alex Moon <moonmeister@users.noreply.github.com>
2021-10-19 00:45:13 -0400

    test(gatsby-plugin-fastify): implement testing (#97)
    * initial testing setup
    
    * Docs/types
    
    * test: implement initial tests, fix path resolution in clientPaths, add splat route for testing
    
    * fix: add pretest to package and execute
    
    * silence logs
    
    * remove console.log from testing
    
    * test: add test for path prefix
    
    * test: write tests for gatsby-node
    
    * add coverage log on test run
    
    * chore: and changeset
    
    * fix: better mocks so things aren't fragile and reading from the FS.
    
    * fix: some snap shot issues

--- packages/gatsby-plugin-fastify/src/gatsby/functionsManifest.ts
+++ packages/gatsby-plugin-fastify/src/gatsby/functionsManifest.ts
@@ -5,18 +5,12 @@ import { PluginData } from "../utils/plugin-data";
 export async function getFunctionManifest(pluginData: PluginData): Promise<IGatsbyFunction[]> {
   const { functionsFolder } = pluginData;
   const compiledFunctionsDir = functionsFolder();
-  let functions: IGatsbyFunction[] = [];
 
   if (!existsSync(compiledFunctionsDir)) {
-    console.error("Unable to find function mainfest @ ", compiledFunctionsDir);
-    return functions;
+    throw new Error(`Unable to find function mainfest @ ${compiledFunctionsDir}`);
   }
 
-  try {
-    functions = await readJSON(functionsFolder(`manifest.json`), `utf-8`);
-  } catch (e) {
-    // ignore
-  }
+  const functions = await readJSON(functionsFolder(`manifest.json`), `utf-8`);
 
   return functions;
 }
