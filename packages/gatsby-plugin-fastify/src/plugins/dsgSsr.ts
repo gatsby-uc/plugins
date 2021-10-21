b076105f20ffe205b666841c6edbc8618114c235
Alex Moon <alex.jared.moon@gmail.com>
2021-10-21 14:08:36 -0700

    Fix ico causeing fastify error

--- packages/gatsby-plugin-fastify/src/plugins/dsgSsr.ts
+++ packages/gatsby-plugin-fastify/src/plugins/dsgSsr.ts
@@ -1,10 +1,12 @@
 import type { FastifyPluginAsync } from "fastify";
 import path from "path";
+import fs from "fs-extra";
 
 import { reverseFixedPagePath } from "gatsby/dist/utils/page-data";
+import { PATH_TO_PUBLIC } from "../utils/constants";
 
 export const handleDsgSsr: FastifyPluginAsync = async (fastify, {}) => {
-  console.info("Listening for DSG and SSR requests");
+  fastify.log.info("Listening for DSG and SSR requests");
 
   const cachePath = path.resolve("./.cache");
 
@@ -20,6 +22,13 @@ export const handleDsgSsr: FastifyPluginAsync = async (fastify, {}) => {
     dbPath: path.join(cachePath, "data", "datastore"),
   });
 
+  const gatsby500ErrorFileExists = fs.existsSync(path.resolve(PATH_TO_PUBLIC, "500.html"));
+  fastify.log.info(
+    `Gatsby 500 error page ${
+      gatsby500ErrorFileExists ? "exists." : "missing. (using generic 500 error for DSG/SSR)"
+    }`,
+  );
+
   // Handle page data for SSR/DSG routes
   fastify.get<{
     Params: {
@@ -28,7 +37,7 @@ export const handleDsgSsr: FastifyPluginAsync = async (fastify, {}) => {
   }>("/page-data/*", async (req, reply) => {
     const requestedPagePath = req.params["*"].replace("/page-data.json", "");
 
-    console.log("DSG/SSR for `page-data.json` @ ", requestedPagePath);
+    fastify.log.debug(`DSG/SSR for "page-data.json" @ ${requestedPagePath}`);
 
     const potentialPagePath = reverseFixedPagePath(requestedPagePath);
     const page = graphqlEngine.findPageByPath(potentialPagePath);
@@ -51,11 +60,16 @@ export const handleDsgSsr: FastifyPluginAsync = async (fastify, {}) => {
 
         reply.send(pageData);
       } else {
+        fastify.log.warn(`DSG/SSR for ${req.url} not found`);
         reply.code(404).send("Page data not found");
       }
     } catch (e) {
-      console.error("Error rendering route", page?.path, e);
-      reply.code(500).sendFile("500.html");
+      fastify.log.error("Error rendering route", page?.path, e);
+      if (gatsby500ErrorFileExists) {
+        reply.code(500).sendFile("500.html");
+      } else {
+        reply.code(500).send("Error rendering route");
+      }
     }
   });
 
@@ -63,9 +77,10 @@ export const handleDsgSsr: FastifyPluginAsync = async (fastify, {}) => {
   fastify.get("*", async (req, reply) => {
     const accept = req.accepts();
     if (accept.types().includes("text/html")) {
-      console.log("DSG/SSR for `text/html` @ ", req.url);
+      fastify.log.debug(`DSG/SSR for "text/html" @  ${req.url}`);
       const potentialPagePath = reverseFixedPagePath(req.url);
       const page = graphqlEngine.findPageByPath(potentialPagePath);
+      reply.header("x-gatsby-fastify", `served-by: ${page?.mode || "dsg/ssr handler"}`);
 
       try {
         if (page && (page.mode === "DSG" || page.mode === "SSR")) {
@@ -81,17 +96,23 @@ export const handleDsgSsr: FastifyPluginAsync = async (fastify, {}) => {
             }
           }
 
-          reply.header("x-gatsby-fastify", `served-by: ${page.mode}`);
           reply.type("text/html").send(results);
-        } else if (req.url === "/favicon.ico") {
-          reply.code(404).send("Not found");
         } else {
+          fastify.log.warn(`DSG/SSR for ${req.url} not found`);
           reply.callNotFound();
         }
       } catch (e) {
-        console.error("Error rendering route", page?.path, e);
-        reply.code(500).sendFile("500.html");
+        fastify.log.error(`Error rendering route @ ${page?.path}: ${e}`);
+        if (gatsby500ErrorFileExists) {
+          reply.code(500).sendFile("500.html");
+        } else {
+          reply.code(500).send("Error rendering route");
+        }
       }
+    } else if (req.url === "/favicon.ico") {
+      reply.code(404).send("Favicon Not found");
+    } else {
+      reply.callNotFound();
     }
   });
 };
