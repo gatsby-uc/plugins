a05f10b9dff8a7dadea9a319cb3f3f2a128276d0
Alex Moon <alex.jared.moon@gmail.com>
2021-10-21 14:08:06 -0700

    fix: handle folks not providing Gatsby 404

--- packages/gatsby-plugin-fastify/src/plugins/404.ts
+++ packages/gatsby-plugin-fastify/src/plugins/404.ts
@@ -1,7 +1,21 @@
 import { FastifyPluginAsync } from "fastify";
+import path from "path";
+import fs from "fs-extra";
+import { PATH_TO_PUBLIC } from "../utils/constants";
 
 export const handle404: FastifyPluginAsync<{}> = async (fastify, _opts) => {
+  const gatsby404ErrorFileExists = fs.existsSync(path.resolve(PATH_TO_PUBLIC, "404.html"));
+  fastify.log.info(
+    `Gatsby 404 error page ${
+      gatsby404ErrorFileExists ? "exists." : "missing. (using generic 404 error)"
+    }`,
+  );
+
   fastify.setNotFoundHandler((_req, reply) => {
-    reply.code(404).sendFile("404.html");
+    if (gatsby404ErrorFileExists) {
+      reply.code(404).sendFile("404.html");
+    } else {
+      reply.code(404).send("Page not found.");
+    }
   });
 };
