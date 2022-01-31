56bbfa99886f3ed11c97463978a7c516286b3856
Alex Moon <moonmeister@users.noreply.github.com>
2022-01-31 09:38:56 -0800

    new(gatsby-source-s3): add to repo (#145)
    * Initial commit and cleanup for s3
    
    * chore: update license date
    
    * chore: add s3 starter, and move fastify starter to new naming convention
    
    * package cleanup
    
    * docs: add @robinmetral as a contributor
    
    * fix yarn lock
    
    * disable tests for now, cleanup example
    
    * fix: tests
    
    * misc cleanup
    
    * chore: move folders and naming

---
+++ packages/gatsby-source-s3/tests/e2e/main.spec.js
@@ -0,0 +1,11 @@
+describe("e2e", () => {
+  it("should source images from S3", () => {
+    cy.visit("http://localhost:9000");
+
+    cy.get("img").should("exist");
+  });
+
+  it("should contain all images", () => {
+    cy.get(".images-grid").find(".s3-image").should("have.length", 1502);
+  });
+});
