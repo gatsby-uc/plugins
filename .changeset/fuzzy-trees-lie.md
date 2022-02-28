3dad05c9802fd83a34cf3c36b11b4f5d2ffe0694
Alex Moon <alex.jared.moon@gmail.com>
2022-02-27 17:33:22 -0800

    feat: Plugin options validation & async query functions

---
+++ .changeset/fuzzy-trees-lie.md
@@ -0,0 +1,6 @@
+---
+"gatsby-source-supabase": minor
+---
+
+- Adding options validation for plugin config.
+- Refactored code to support async `query` Functions.
--- packages/gatsby-source-supabase/gatsby-node.js
+++ packages/gatsby-source-supabase/gatsby-node.js
@@ -1,30 +1,60 @@
 const { createClient } = require("@supabase/supabase-js");
 
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    supabaseUrl: Joi.string()
+      .uri({
+        allowRelative: false,
+      })
+      .required(),
+    supabaseKey: Joi.string().required(),
+    types: Joi.array()
+      .items(
+        Joi.object({
+          type: Joi.string().required(),
+          query: Joi.function().arity(1).required(),
+        })
+      )
+      .required()
+      .min(1),
+  });
+};
+
 exports.sourceNodes = async (
-  { actions, createNodeId, createContentDigest },
+  { actions, createNodeId, createContentDigest, reporter },
   { supabaseUrl, supabaseKey, types }
 ) => {
   const { createNode } = actions;
 
   const supabase = createClient(supabaseUrl, supabaseKey);
 
-  types.forEach(({ type, query }) => {
-    query(supabase).then(({ data }) => {
-      data.forEach((entry) => {
+  for (const type of types) {
+    const { type: typeName, query } = type;
+
+    try {
+      const { data, error } = await query(supabase);
+
+      if (!Array.isArray(data) && !data?.length > 0) {
+        reporter.panicOnBuild("No data found for type '" + typeName + "'", error);
+        continue;
+      }
+
+      for (const item of data) {
         createNode({
-          ...entry,
-          id: createNodeId(`${type}-${entry.id}`),
+          ...item,
+          databaseId: item.id,
+          id: createNodeId(`supabase-${typeName}-${item.id}`),
           parent: null,
           children: [],
           internal: {
-            type,
-            content: JSON.stringify(entry),
-            contentDigest: createContentDigest(entry),
+            type: `Supabase${typeName}`,
+            content: JSON.stringify(item),
+            contentDigest: createContentDigest(item),
           },
         });
-      });
-    });
-  });
-
-  return;
+      }
+    } catch (e) {
+      reporter.panic("Error sourcing nodes data for type '" + typeName + "'", e);
+    }
+  }
 };
