178172af17a4b0709a8d747d098fc543ef9629c0
Alex Moon <alex.jared.moon@gmail.com>
2022-04-20 10:51:30 -0500

    chore: clarify boot mesage for transforms

--- packages/gatsby-plugin-fastify/src/plugins/imageTransform.ts
+++ packages/gatsby-plugin-fastify/src/plugins/imageTransform.ts
@@ -3,6 +3,6 @@ import { addImageRoutes } from "gatsby-plugin-utils/polyfill-remote-file";
 import type { FastifyPluginAsync } from "fastify";
 
 export const handleImageTransforms: FastifyPluginAsync = async (fastify) => {
-  fastify.log.debug(`📷  Handling file transforms`);
+  fastify.log.debug(`📷  Handling file/image transforms aka "Gatsby Image CDN"`);
   fastify.use(addImageRoutes);
 };
