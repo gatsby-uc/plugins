caf0984985b1c27598ff897bc275bdd5edc959f5
mattcompiles <mattjones701@gmail.com>
2022-09-23 20:33:28 +1000

    BREAKING(gatsby-plugin-vanilla-extract): Remove Babel plugin & bump webpack peerDep (#254)
    Co-authored-by: Lennart <lekoarts@gmail.com>

--- packages/gatsby-plugin-vanilla-extract/gatsby-node.js
+++ packages/gatsby-plugin-vanilla-extract/gatsby-node.js
@@ -1,11 +1,5 @@
 const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
 
-exports.onCreateBabelConfig = ({ actions }, pluginOptions) => {
-  actions.setBabelPlugin({
-    name: require.resolve(`@vanilla-extract/babel-plugin`),
-  });
-};
-
 exports.onCreateWebpackConfig = ({ actions }, pluginOptions) => {
   actions.setWebpackConfig({
     plugins: [new VanillaExtractPlugin(pluginOptions)],
