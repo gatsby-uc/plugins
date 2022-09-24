d7ace2dc1076feb3ef60e5c132cd86c8701c6908
Martin Klingenberg <martin@klingenberg.as>
2022-09-24 19:27:13 +0200

    Fallback routes get SSR-support 📦 (#253)
    docs: add @klyngen as a contributor
    
    Co-authored-by: Martin Klingenberg <martin.klingenberg@alv.no>

--- packages/gatsby-plugin-fastify/src/gatsby/serverRoutes.ts
+++ packages/gatsby-plugin-fastify/src/gatsby/serverRoutes.ts
@@ -1,17 +1,18 @@
 import type { PluginData } from "../utils/plugin-data";
+import { formatMatchPath } from "../utils/routes";
 
 export type ServerSideRoute = { path: string; mode: "DSG" | "SSR" };
 
 export async function getServerSideRoutes(pageData: PluginData) {
   const { pages } = pageData;
-
   const routes: ServerSideRoute[] = [];
 
   for (const page of pages.values()) {
     if (page?.mode === "DSG" || page?.mode === "SSR") {
-      const { path, mode } = page;
+      const { path, mode, matchPath } = page;
+
       routes.push({
-        path,
+        path: formatMatchPath(matchPath) ?? path,
         mode,
       });
     }
