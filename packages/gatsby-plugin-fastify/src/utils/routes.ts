96454e7bedb026b7a074cad510f98233924e4e0d
Thomas Dexter <tsdexter@gmail.com>
2022-11-04 23:12:40 -0400

    signify `match` is not unused
    Co-authored-by: Alex Moon <moonmeister@users.noreply.github.com>

--- packages/gatsby-plugin-fastify/src/utils/routes.ts
+++ packages/gatsby-plugin-fastify/src/utils/routes.ts
@@ -11,8 +11,8 @@ export function removeQueryParmsFromUrl(url: string) {
 }
 
 export function buildUrlFromParams(path: string, data: { [s: string]: string } = {}) {
-  return path.replace(/:(\w+)|(\*)/gi, function (_match, p1, p2) {
-    if (p1 && !data[p1]) return _match; // :Something in toPath does not have a splat in fromPath pass it through colon intact
+  return path.replace(/:(\w+)|(\*)/gi, function (match, p1, p2) {
+    if (p1 && !data[p1]) return match; // :Something in toPath does not have a splat in fromPath pass it through colon intact
     let lookupString = p1 ?? p2;
     let replacement = data[lookupString];
 
