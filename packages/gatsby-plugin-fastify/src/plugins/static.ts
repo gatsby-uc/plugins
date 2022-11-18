25d5a063af2b80c675671b58112382f88de15a1f
AJ Moon <alex.jared.moon@gmail.com>
2022-11-18 21:26:48 +0000

    final static fix...hopefully

--- packages/gatsby-plugin-fastify/src/plugins/static.ts
+++ packages/gatsby-plugin-fastify/src/plugins/static.ts
@@ -21,9 +21,9 @@ export const handleStatic: FastifyPluginAsync<Partial<FastifyStaticOptions>> = f
           isMatch(path, ["**/public/*.@(js|css)", "**/public/static/**"]) &&
           isMatch(path, "!**/sw.js")
         ) {
-          reply.setHeaders(...IMMUTABLE_CACHING_HEADER);
+          reply.setHeader(...IMMUTABLE_CACHING_HEADER);
         } else {
-          reply.setHeaders(...NEVER_CACHE_HEADER);
+          reply.setHeader(...NEVER_CACHE_HEADER);
         }
         appendModuleHeader("Static", reply);
       },
