5fef2c9e7c3b38cd37cd743e757ba49588d3d52b
Alex Moon <moonmeister@users.noreply.github.com>
2023-08-27 23:24:58 +0000

    implement dynamic routes

---
+++ packages/gatsby-adapter-fastify/src/plugins/dynamic.ts
@@ -0,0 +1,81 @@
+import { resolve } from "node:path";
+import { pathExists } from "fs-extra";
+import { StatusCodes } from "http-status-codes";
+
+import middiePlugin from "@fastify/middie";
+
+import type { FastifyPluginAsync } from "fastify";
+import {
+  IFunctionDefinition,
+  IFunctionRoute,
+  GatsbyFunctionRequest,
+  GatsbyFunctionResponse,
+} from "gatsby";
+
+export type GatsbyFunctionHandler = (
+  request: GatsbyFunctionRequest,
+  reply: GatsbyFunctionResponse
+) => void | Promise<void>;
+
+async function importFunction(
+  pathToEntryPoint: string
+): Promise<GatsbyFunctionHandler | undefined> {
+  const functionImportAbsPath = resolve(pathToEntryPoint);
+
+  if (!pathExists(functionImportAbsPath)) {
+    throw new Error(`Unable to find function to import @ ${functionImportAbsPath}`);
+  }
+
+  const functionExec = await import(functionImportAbsPath);
+
+  return functionExec?.default ?? functionExec; // Fixes export issues related to default export
+}
+
+export const handleDynamic: FastifyPluginAsync<{
+  routes: IFunctionRoute[];
+  functions: IFunctionDefinition[];
+}> = async (fastify, { routes, functions }) => {
+  await fastify.register(middiePlugin);
+
+  fastify.log.info(`Registering ${functions.length} function(s) for ${routes.length} routes`);
+
+  const availableFunctions: { [key: string]: GatsbyFunctionHandler } = {};
+
+  for (const { functionId, pathToEntryPoint } of functions) {
+    try {
+      const functionFunction = await importFunction(pathToEntryPoint);
+
+      if (functionFunction) {
+        fastify.log.debug(`Registering function:  ${functionId}`);
+
+        availableFunctions[functionId] = functionFunction;
+      }
+    } catch (error) {
+      fastify.log.error(error);
+    }
+  }
+
+  for (const { functionId, path, cache } of routes) {
+    const functionToExecute = availableFunctions[functionId];
+
+    fastify.all(path, {
+      handler: async function (request, reply) {
+        try {
+          reply.appendModuleHeader("Dynamic");
+          await Promise.resolve(functionToExecute(request, reply));
+        } catch (error) {
+          fastify.log.error(error);
+          // Don't send the error if that would cause another error.
+          if (!reply.sent) {
+            reply.code(StatusCodes.INTERNAL_SERVER_ERROR).send("Error executing Gatsby Function.");
+          }
+        }
+      },
+    });
+  }
+
+  // TODO  Do I need this for 404 handling? I don't think so but need to be sure
+  // fastify.all("/*", async (_request, reply) => {
+  //   reply.code(StatusCodes.NOT_FOUND).send("Function not found.");
+  // });
+};
