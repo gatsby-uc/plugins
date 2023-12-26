4e87f84f06f0ac4aef1b8c5d6648dc8fb0eda2a7
AJ Moon <alex.jared.moon@gmail.com>
2023-12-26 03:17:15 +0000

    fix: add extended timeout for tests to get them to pass

--- packages/gatsby-plugin-fastify/src/__tests__/__utils__/setup-file.js
+++ packages/gatsby-plugin-fastify/src/__tests__/__utils__/setup-file.js
@@ -2,7 +2,7 @@ import { setupFastify, shutdownFastify } from "./server";
 
 beforeAll(async () => {
   globalThis.fastify = await setupFastify();
-});
+}, 100_000);
 
 afterAll(async () => {
   await shutdownFastify(globalThis.fastify);
