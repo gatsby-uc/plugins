a55e05f6e72da09dd0302cb6d13f5cd34ea3cf51
AJ Moon <alex.jared.moon@gmail.com>
2023-12-26 03:36:24 +0000

    20 second timeout

--- packages/gatsby-plugin-fastify/src/__tests__/__utils__/setup-file.js
+++ packages/gatsby-plugin-fastify/src/__tests__/__utils__/setup-file.js
@@ -2,7 +2,7 @@ import { setupFastify, shutdownFastify } from "./server";
 
 beforeAll(async () => {
   return (globalThis.fastify = await setupFastify());
-}, 10_000);
+}, 30_000);
 
 afterAll(async () => {
   return shutdownFastify(globalThis.fastify);
