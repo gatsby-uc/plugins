738c287a52a034db4d2f78c6066b83736f4a2090
AJ Moon <alex.jared.moon@gmail.com>
2023-12-27 23:14:29 +0000

    no idea what's happening at this point. trying shit

--- packages/gatsby-plugin-fastify/src/__tests__/__utils__/setup-file.js
+++ packages/gatsby-plugin-fastify/src/__tests__/__utils__/setup-file.js
@@ -1,9 +1,8 @@
 import { setupFastify, shutdownFastify } from "./server";
 
 beforeAll(async () => {
-  jest.setTimeout(30_000);
   return (globalThis.fastify = await setupFastify());
-});
+}, 100_000);
 
 afterAll(async () => {
   return shutdownFastify(globalThis.fastify);
