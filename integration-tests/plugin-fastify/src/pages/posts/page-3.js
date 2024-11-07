9fe589488dc8e10596678b8fee64b9497323adf5
Alex Moon <moonmeister@users.noreply.github.com>
2024-11-06 16:32:35 -0800

    deprecate: remove all packages related to gatsby plugin fastify

--- integration-tests/plugin-fastify/src/pages/posts/page-3.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import PostPage from "../../components/Post";
-
-export default function postPage3() {
-  return <PostPage title="Page 3" content="Page 3 content" />;
-}
